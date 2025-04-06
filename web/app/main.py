c54237d7d0464adda69d6451d5313e19599558da
JavierLlanos <javierllanosatoy@gmail.com>
2025-04-06 18:02:15 -0500

    Recuperando cambios

--- web/app/main.py
+++ web/app/main.py
@@ -1,1 +1,12 @@
-# web/app/main.py
+from flask import Flask, render_template, redirect
+import sqlite3
+from pprint import pprint
+
+
+conexion = sqlite3.connect('web2.sqlite3')
+conexion.row_factory = sqlite3.Row #modo diccionario
+cursor = conexion.cursor()
+
+
+# aplicación
+app = Flask(__name__)
